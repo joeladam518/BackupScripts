0164dee24c84f3ccf667c1643657c5712f5685c3
Joel Haker <joeladam@gmail.com>
2023-01-07 05:12:55 -0500

    update rsync backup script

--- RsyncBackup.sh
+++ RsyncBackup.sh
@@ -1,73 +1,108 @@
 #!/usr/bin/env bash
 
+set -Eeo pipefail
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null 2>&1 && pwd -P)"
+
 # Functions
-displayHelp()
-{
-    echo "Usage: ${scriptName} [-t] [-h] [-z] {Files|Media}"
-    echo "  -h  Display help message."
-    echo "  -t  Testing Mode adds (-n) to the rsync command."
-    echo "  -z  Compress files during transfer."
-} 
+usage() {
+    cat << EOF
+Usage: $(basename "${0}") [-h] [-t] [-z] [-f] {files|media}
 
-# logging
-script_start="$(date "+%Y-%m-%dT%H%M%S%:::z")"
-log_file="${HOME}/${script_start}_rsync_log.txt"
+Backup your NAS
 
-# directories
-remote_dir="/mnt/main"
-local_dir="/mnt/i"
+Options:
+-h, --help      Print this help and exit
+-t, --test      Testing mode. Adds (-n) to the rsync command
+-z, --zip       Compress files during transfer
+EOF
+    return 0
+}
 
-# Flags
-testing_mode="0"
-compress_durring_transfer="0"
+confirm() {
+    local prompt answer response
+    prompt="$(echo -e "$*")"
+    read -er -t 10 -p "$prompt" answer
+    for response in y Y yes Yes YES sure Sure SURE ok Ok OK
+    do
+        if [ "_$answer" == "_$response" ]; then
+            return 0
+        fi
+    done
 
-# Options
-while getopts ":htz" opt; do
-    case ${opt} in
-        t)  
-            testing_mode="1"
-            ;;
-        z)  
-            compress_durring_transfer="1" 
-            ;;
-        h)  
-            displayHelp 
-            exit 0
-            ;;
-        \?) 
-            echo "Invalid Option: -${OPTARG}" 1>&2
-            exit 1
-            ;;
+    return 1
+}
+
+invalid_argument() {
+    echo "Invalid option: -${1} requires an argument" 1>&2
+    exit 1
+}
+
+invalid_option() {
+    echo "Invalid option: -${1}" 1>&2
+    exit 1
+}
+
+parse_long_options() {
+    case "$1" in
+        help) usage; exit ;;
+        test) testing_mode=1 ;;
+        zip) compress_durring_transfer=1 ;;
+        *) invalid_option "-${OPTARG}" ;;
     esac
-done
 
-shift $((OPTIND-1))
+    return 0
+}
 
-# Input
-scriptName="$0"
-what_to_backup="$1"
+parse_args() {
+    # flags
+    testing_mode=0
+    compress_durring_transfer=0
+
+    while getopts ":htz-:" opt; do
+        case "$opt" in
+            h) usage; exit ;;
+            t) testing_mode=1 ;;
+            z) compress_durring_transfer=1 ;;
+            :) invalid_argument "$OPTARG" ;;
+            -) parse_long_options "$OPTARG" ;;
+            \?) invalid_option "$OPTARG" ;;
+        esac
+    done
+
+    shift $((OPTIND - 1))
+
+    # args
+    backup_directory="${1:-""}"
+
+    return 0
+}
 
 # Start script
-if [ ! -d "$local_dir" ]; then
-    echo "Can not back up to \"$local_dir\" it does not exist."
-    exit 1 
-fi
+parse_args "$@"
 
-if [ -z "$what_to_backup" ]; then
-    echo "You must tell me what to back up."
-    displayHelp
+log_file="${SCRIPT_DIR}/$(date "+%Y-%m-%dT%H%M%S%:::z")_rsync_log.txt"
+exclude_path="${SCRIPT_DIR}/exclude-from.txt"
+local_path="/mnt/i"
+remote_path="/mnt/main/${backup_directory}"
+
+if [ -z "$backup_directory" ] || { [ "$backup_directory" != "files" ] && [ "$backup_directory" != "media" ]; }; then
+    echo "Invalid directory name: \"${backup_directory}\"" 1>&2
     exit 1
 fi
 
-if [ "$what_to_backup" != "files" ] && [ "$what_to_backup" != "media" ]; then
-    echo "Invalid Argument: ${what_to_backup}" 
-    displayHelp
+if [ ! -d "$local_path" ]; then
+    echo "\"$local_path\" doesn't exist" 1>&2
     exit 1
 fi
 
-remote_dir="${remote_dir}/${what_to_backup}"
+if [ ! -f "$exclude_path" ]; then
+    echo "\"$exclude_path\" doesn't exist" 1>&2
+    exit 1
+fi
 
 # Build the rsync command
+#----------------------------------------------------------------------------------------------------------------------
 backup_cmd="rsync"
 
 if [ "$testing_mode" == "1" ]; then
@@ -78,21 +113,20 @@ if [ "$compress_durring_transfer" == "1" ]; then
     backup_cmd="${backup_cmd} -z"
 fi
 
-backup_cmd="${backup_cmd} -ahP -vv --delete-delay --log-file=${log_file}" # --update
-backup_cmd="${backup_cmd} --exclude=.recycle --exclude=System\ Volume\ Information"
-backup_cmd="${backup_cmd} jhaker@freenas.jhaker.net:${remote_dir} ${local_dir}"
+backup_cmd="${backup_cmd} -ahP -vv --delete-delay --log-file=\"${log_file}\" --exclude-from=\"${exclude_path}\""
+backup_cmd="${backup_cmd} jhaker@freenas.jhaker.net:${remote_path} ${local_path}"
+#----------------------------------------------------------------------------------------------------------------------
 
-# Confirm, then run or not...
+echo
 echo "$backup_cmd"
-read -p "Run command (Yes/No)? " -t 15  answer
+echo
 
-case ${answer} in
-    [Yy]* ) eval "$backup_cmd" ;;
-    [Nn]* ) exit 0 ;;
-    * ) echo "Please answer yes or no."; exit 1 ;;
-esac
+if confirm "Run command? (yes/No):\n> "; then
+    eval "$backup_cmd"
 
-script_end="$(date "+%Y-%m-%dT%H:%M:%S%:::z")"
-echo "" >> "$log_file"
-echo "Script ended at:   ${script_end}" >> "$log_file"
-echo "" >> "$log_file"
+    echo "" >> "$log_file"
+    echo "Script ended at:   $(date "+%Y-%m-%dT%H:%M:%S%:::z")" >> "$log_file"
+    echo "" >> "$log_file"
+else
+    echo "Exiting..."
+fi
